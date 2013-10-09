fc235369dd02e6840292e25e8593381e349be474
Peter Baker <peter@finack.com>
2013-10-08 23:14:36 -0700

    Check for nil values
    If you have an empty queue, most of these values will not be present
    in the `queue`. Check to see if there is a proper value and otherwise
    return a 0 (vs blowing up or nil)

--- rabbitmq_queue_details/rabbitmq_queue_details.rb
+++ rabbitmq_queue_details/rabbitmq_queue_details.rb
@@ -31,20 +31,23 @@ class RabbitmqQueueDetails < Scout::Plugin
     queue = get_queue(option(:vhost), option(:queue))
     raise SecurityError.new(queue["reason"]) if queue.has_key? "error"
 
+    report(:messages                => value_or_zero(queue["messages"]),
+           :messages_unacknowledged => value_or_zero(queue["messages_unacknowledged"]),
+           :memory                  => value_or_zero(queue["memory"].to_f / (1024 * 1024)),
+           :pending_acks            => value_or_zero(queue["backing_queue_status"]["pending_acks"]),
+           :consumers               => value_or_zero(queue["consumers"]),
+           :durable                 => queue["durable"] ? 1 : 0,
+           :messages_ready          => value_or_zero(queue["messages_ready"]))
 
-    report(:messages => queue["messages"],
-           :messages_unacknowledged => queue["messages_unacknowledged"],
-           :memory => queue["memory"].to_f / (1024 * 1024),
-           :pending_acks => queue["backing_queue_status"]["pending_acks"],
-           :consumers => queue["consumers"],
-           :durable => queue["durable"] ? 1 : 0,
-           :messages_ready => queue["messages_ready"])
   rescue Errno::ECONNREFUSED
     error("Unable to connect to RabbitMQ Management server", "Please ensure the connection details are correct in the plugin settings.\n\nException: #{$!.message}\n\nBacktrace:\n#{$!.backtrace}")
   rescue SecurityError => e
     error("Server returned an error\nException: #{e.message}\n\nBacktrace:\n#{e.backtrace.join("\n")}")
   end
 
+  def value_or_zero(val)
+    val.nil? ? 0 : val
+  end
 
   def get_queue(vhost, queue)
     url = "#{option('management_url').to_s.strip}/api/queues/#{CGI::escape(vhost)}/#{queue}/"
