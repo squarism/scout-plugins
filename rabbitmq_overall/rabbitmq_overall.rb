58f0f60b777c8d632c3793e26eeb35869191f855
Peter Baker <peter@finack.com>
2013-10-08 21:59:14 -0700

    Obtaining nodes information is optional
    If the Rabbit MQ user does not have the monitoring permission, they
    can not access this API & information.

--- rabbitmq_overall/rabbitmq_overall.rb
+++ rabbitmq_overall/rabbitmq_overall.rb
@@ -17,18 +17,28 @@ class RabbitmqOverall < Scout::Plugin
     password:
         default: guest
         attributes: password
- EOS
+    monitor_user:
+        default: true
+        notes: Is username a monitor user?
+  EOS
 
   def build_report
     overview = get('overview')
-    nodes = get('nodes')
 
-    report(:bindings => get('bindings').length,
-           :connections => get('connections').length,
-           :queues => get('queues').length,
-           :queue_memory_used => nodes[0]["mem_used"].to_f / (1024 * 1024),
-           :messages => (overview["queue_totals"].any? ? overview["queue_totals"]["messages"] : 0),
-           :exchanges => get('exchanges').length)
+    results = {
+      :bindings => get('bindings').length,
+      :connections => get('connections').length,
+      :queues => get('queues').length,
+      :messages => (overview["queue_totals"].any? ? overview["queue_totals"]["messages"] : 0),
+      :exchanges => get('exchanges').length
+    }
+
+    if option(:monitor_user)
+      nodes = get('nodes')
+      results[:queue_memory_used] = nodes[0]["mem_used"].to_f / (1024 * 1024)
+    end
+
+    report(results)
   rescue Errno::ECONNREFUSED
     error("Unable to connect to RabbitMQ Management server", "Please ensure the connection details are correct in the plugin settings.\n\nException: #{$!.message}\n\nBacktrace:\n#{$!.backtrace}")
   end
