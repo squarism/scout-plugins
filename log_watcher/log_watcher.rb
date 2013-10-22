82fe1b0332b4a8f64467979c2efeff7701d4a141
Kevin Lawver <lawver@gmail.com>
2013-10-22 08:53:31 -0400

    Adds a sudo option for the log watcher.

--- log_watcher/log_watcher.rb
+++ log_watcher/log_watcher.rb
@@ -15,15 +15,25 @@ class LogWatcher < Scout::Plugin
     attributes: advanced
     default: 1
     notes: 1=yes
+  use_sudo:
+    attributes: advanced
+    default: 0
+    notes: 1=use sudo
   EOS
   
   def init
+    if option('use_sudo').to_i == 1
+      @sudo_cmd = "sudo "
+    else
+      @sudo_cmd = ""
+    end
+
     @log_file_path = option("log_path").to_s.strip
     if @log_file_path.empty?
       return error( "Please provide a path to the log file." )
     end
     
-    `test -e #{@log_file_path}`
+    `#{@sudo_cmd}test -e #{@log_file_path}`
     
     unless $?.success?
       error("Could not find the log file", "The log file could not be found at: #{@log_file_path}. Please ensure the full path is correct.") if option("send_error_if_no_log") == "1"
@@ -41,7 +51,7 @@ class LogWatcher < Scout::Plugin
     return if init()
     
     last_bytes = memory(:last_bytes) || 0
-    current_length = `wc -c #{@log_file_path}`.split(' ')[0].to_i
+    current_length = `#{@sudo_cmd}wc -c #{@log_file_path}`.split(' ')[0].to_i
     count = 0
     elapsed_seconds = 0
     # don't run it the first time
@@ -53,7 +63,7 @@ class LogWatcher < Scout::Plugin
         # finds new content from +last_bytes+ to the end of the file, then just extracts from the recorded 
         # +read_length+. This ignores new lines that are added after finding the +current_length+. Those lines 
         # will be read on the next run.
-        count = `tail -c +#{last_bytes+1} #{@log_file_path} | head -c #{read_length} | grep "#{@term}" -#{option(:grep_options).to_s.gsub('-','')}c`.strip.to_f
+        count = `#{@sudo_cmd}tail -c +#{last_bytes+1} #{@log_file_path} | head -c #{read_length} | grep "#{@term}" -#{option(:grep_options).to_s.gsub('-','')}c`.strip.to_f
         # convert to a rate / min
         elapsed_seconds = Time.now - @last_run
         count = count / (elapsed_seconds/60)
