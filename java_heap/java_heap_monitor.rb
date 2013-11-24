1e0218a74afca6eeec3f66339236d4139b17e291
Ken Everett <ken.everett@gmail.com>
2013-11-23 21:36:06 -0800

    handling more integration issues now

--- java_heap/java_heap_monitor.rb
+++ java_heap/java_heap_monitor.rb
@@ -7,26 +7,77 @@ class JavaHeapMonitor < Scout::Plugin
 
   def build_report
     begin
-      report(:heap => heap_size)
+      
+      ## TODO: work even if the string Total is found twice
+      
+      pid = grep_java_process
+      if pid.lines.count > 1
+        error("too many processes", "more than one process was found grepping for java.") 
+      elsif pid.lines.count < 1
+        error("no java process", "no process was found grepping for java") 
+      elsif !File.exist?(jmap) 
+        error("jmap missing", "jmap executable #{jmap} not found")         
+      elsif !File.executable?(jmap) 
+        error("jmap not executable", "jmap #{jmap} not executable")         
+      else
+        histo_output = jmap_histo(pid)
+        if histo_output.index('Total') == nil
+          error("unexpected jmap output from #{jmap} -histo:live #{pid}", "This plugin expects the output of jmap from jdk 1.6.0_37 (and hopefully most others as well)  The \'jmap -histo:live\' includes the total heap used in the last line which starts with the string \'Total\'")         
+        else
+          totals = parse_jmap_total(histo_output)
+          if totals.size != 2
+            error("unexpected jmap output from #{jmap} -histo:live #{pid}", "This plugin expects the output of jmap from jdk 1.6.0_37 (and hopefully most others as well)  The \'jmap -histo:live\' includes the total, which should look like \'Total       5081856      493072400\'\nInstead, we parsed as the the total heap used is not an integer: \'#{totals}\' ")         
+          else
+            heap_size = totals[1].chomp
+            if !a_stringified_integer?(heap_size)
+              error("unexpected jmap output from #{jmap} -histo:live #{pid}", "This plugin expects the output of jmap from jdk 1.6.0_37 (and hopefully most others as well)  The \'jmap -histo:live\' includes the total, which should look like \'Total       5081856      493072400\'\nInstead, we parsed as the the total heap used is not an integer: \'#{heap_size}\' ")         
+            else
+             report(:heap => btye_to_mb(heap_size.to_i)) # return mb
+            end
+        end
+      end
+    end
     rescue StandardError => trouble
       error "#{trouble} #{trouble.backtrace}"
     end
   end
+ 
+  def grep_java_process
+    # pid of java process. assumes only 1 java process
+    `ps -eaf | grep java | grep -v grep | grep -v java_heap | awk '{print $2}'`
+  end
 
-  def heap_size
-     # pid of java process. assumes only 1 java process
-     pid = `ps -eaf | grep java | grep -v grep | grep -v java_heap | awk '{print $2}'`
-     # histo output of jmap, which loses all the newlines.  not sure why
-     histo = `#{jmap} -histo:live #{pid} `
+  def jmap_histo(pid)
+    # histo output of jmap, which loses all the newlines.  not sure why
+    histo = `#{jmap} -histo:live #{pid} `
+  end
+
+  def parse_jmap_total(histo)
      # parse out last line with total instance count and heap size in bytes
-     count_size = histo.split('Total', 2)[1]
-     # only return the heap size
-     size = count_size.split(' ')[1]
-     return size.chomp.to_f / 1024 / 1024 # return mb
-   end
+     totals_string = histo.split('Total', 2)[1]
+     totals_string.split(' ')
+  end
+
+  def parse_heap_size(totals)
+    # only return the heap size
+    size = totals[1]
+    return size.chomp.to_f / 1024 / 1024 # return mb
+  end
+
+  def a_stringified_integer?(i)
+    Integer(i)
+    return true
+  rescue ArgumentError
+    return false
+  end
+  
+  def btye_to_mb(i)
+    i.to_f / 1024 / 1024
+  end
 
    def jmap
-     option(:jmap_absolute_path) || '/usr/bin/jmap'
+#     option(:jmap_absolute_path) || '/usr/bin/jmap'
+     option(:jmap_absolute_path) || '/tmp/jmap'
    end
 
 end
