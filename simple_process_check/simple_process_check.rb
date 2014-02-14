squarism/scout-plugins

commit 284dac83da9b135b98a18c52f328d07fe17dd37c

ignore current plugin process
Necessary if checking 'ruby' processes, which matches the plugin process itself, thus giving false positives even if the checked processes aren't running.

simple_process_check/simple_process_check.rb
@@ -38,6 +38,13 @@ class SimpleProcessCheck < Scout::Plugin
     #   ["proxymap", "proxymap -t unix -u"],
     #   ["apache2", "usr/sbin/apache2 -k start"] ]
     ps_output=ps_output.downcase.split("\n").map{|line| line.split(/\s+/,2)}
+    
+    # exclude current process ID from results
+    current_pid_output = `ps -eo pid,comm,args`.downcase.split("\n").detect{|line| line =~ /^#{Process.pid}/}
+    if current_pid_output && current_pid_output.first
+      current_pid_output = current_pid_output.first.map{|line| line.split(/\s+/,3)[1..2]}
+      ps_output -= current_pid_output
+    end
 
     processes_to_watch = process_names.split(",").uniq
     process_counts = processes_to_watch.map do |p|

commit ce8ab2eb3a87dc0f639e39b4acfaa3675e4591c0

Simple Process Check plugin fix
- https://github.com/scoutapp/scout-plugins/commit/284dac83da9b135b98a18c52f328d07fe17dd37c
broke this plugin on new installs

- The issue has been fixed and the code is much simpler with the same
  results

simple_process_check/simple_process_check.rb
@@ -33,18 +33,11 @@ class SimpleProcessCheck < Scout::Plugin
       return error("Couldn't use `ps` as expected.", error.message)
     end
 
-    # this makes ps_output an array of two-element arrays:
+    # This makes ps_output an array of two-element arrays (excluding current process ID):
     # [ ["smtpd", "smtpd -n smtp -t inet -u -c"],
     #   ["proxymap", "proxymap -t unix -u"],
     #   ["apache2", "usr/sbin/apache2 -k start"] ]
-    ps_output=ps_output.downcase.split("\n").map{|line| line.split(/\s+/,2)}
-    
-    # exclude current process ID from results
-    current_pid_output = `ps -eo pid,comm,args`.downcase.split("\n").detect{|line| line =~ /^#{Process.pid}/}
-    if current_pid_output
-      current_pid_output = current_pid_output.split(/\s+/,3)[1..2]
-      ps_output.delete_if { |process| process.first == current_pid_output.first && process.last.strip == current_pid_output.last.strip }
-    end
+    ps_output=ps_output.downcase.split("\n").reject{|line| line =~ /^\s*#{Process.pid}\s+/ }.map{|line| line.split(/\s+/,2)}
 
     processes_to_watch = process_names.split(",").uniq
     process_counts = processes_to_watch.map do |p|
@@ -78,7 +71,7 @@ class SimpleProcessCheck < Scout::Plugin
 
     report(:processes_present => num_processes_present)
   end
-  
+
   # True if a full match OR a match w/a colon appended to the name. Handles cases like:
   # sshd: ubuntu (so 'sshd' will match)
   def process_name_match?(output,name)