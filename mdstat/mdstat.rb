62d94b95ddd5ee0f83faaf0bb952fe50b6c16b73
Mr. Matt <matt@composition9.com>
2014-02-27 13:14:40 -0700

    mdstat plugin monitor more that just the first RAID array

--- mdstat/mdstat.rb
+++ mdstat/mdstat.rb
@@ -4,41 +4,60 @@
 # Created by Mark Hasse on 2008-04-15.
 # =================================================================================
 class MdStat < Scout::Plugin
+
+  OPTIONS = <<-EOS
+    monitor_multiple:
+      label: Monitor Multiple Arrays
+      notes: by default, this plugin only monitors the first/only RAID array it encounters in mdstat. Enter 'true' to monitor all reporting arrays.
+      default: false
+  EOS
+  
   def build_report
     data = Hash.new
          
-    mdstat = %x(cat /proc/mdstat).split(/\n/)
-    
-    spares = mdstat[1].scan(/\(S\)/).size
-    failed = mdstat[1].scan(/\(F\)/).size
+    full_response = %x(cat /proc/mdstat)
+    stripped_response = full_response.chop.split(/\n/)[1..-2].join("\n") # strip off the first and last lines
+    mdstat_arrays = stripped_response.split(/\n\n/)
+    mdstat_arrays = Array(mdstat_arrays.first) unless(option(:monitor_multiple) == 'true')
 
-    mdstat[2] =~ /\[(\d*\/\d*)\].*\[(.+)\]/
-    counts = $1
-    if counts.nil?
-      return error("Not applicable for RAID 0", "This plugin reports the number of active disks, spares, and failed disks. As RAID 0 isn't redundent, a single drive failure destroys the Array. These metrics aren't applicable for RAID 0.")
-    end
-    status = $2
+    data[:active_disks] = 0
+    data[:spares] = 0
+    data[:failed_disks] = 0
     
-    disk_counts = counts.split('/').map { |x| x.to_i } 
-    disk_status = status.squeeze
-    
-    if disk_counts[0].class == Fixnum && disk_counts[1].class == Fixnum
-      data[:active_disks] = disk_counts[0]
-      data[:spares]       = spares
-      data[:failed_disks] = failed
-    else
-      raise "Unexpected mdstat file format"
-    end 
-    
-    if disk_counts[0] != disk_counts[1] || disk_status != 'U' || failed > 0 
-      if memory(:mdstat_ok)
-        remember(:mdstat_ok,false)
-        alert(:subject => 'Disk failure detected')
+    mdstat_arrays.each do |mdstat|
+      mdstat_lines = mdstat.split(/\n/)
+      spares = mdstat_lines[0].scan(/\(S\)/).size
+      failed = mdstat_lines[0].scan(/\(F\)/).size
+
+      mdstat_lines[1] =~ /\[(\d*\/\d*)\].*\[(.+)\]/
+      counts = $1
+      if counts.nil?
+        error("Not applicable for RAID 0", "This plugin reports the number of active disks, spares, and failed disks. As RAID 0 isn't redundent, a single drive failure destroys the Array. These metrics aren't applicable for RAID 0.")
+        next
       end
-    else
-      remember(:mdstat_ok,true)
+      status = $2
+      
+      disk_counts = counts.split('/').map { |x| x.to_i } 
+      disk_status = status.squeeze
+      
+      if disk_counts[0].class == Fixnum && disk_counts[1].class == Fixnum
+        data[:active_disks] += disk_counts[0]
+        data[:spares]       += spares
+        data[:failed_disks] += failed
+      else
+        raise "Unexpected mdstat file format"
+      end 
+      
+      if disk_counts[0] != disk_counts[1] || disk_status != 'U' || failed > 0 
+        if memory(:mdstat_ok)
+          remember(:mdstat_ok,false)
+          alert(:subject => 'Disk failure detected')
+        end
+      else
+        remember(:mdstat_ok,true)
+      end
     end
-
     report(data)
   end
 end
+
--- mdstat/test.rb
+++ mdstat/test.rb
@@ -2,21 +2,38 @@ require File.expand_path('../../test_helper.rb', __FILE__)
 require File.expand_path('../mdstat.rb', __FILE__)
 
 class MdStatTest < Test::Unit::TestCase
-    def test_success      
-      plugin=MdStat.new(nil,{},{})
-      plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__)+'/fixtures/proc_mdstat_raid5.txt'))
+  def test_success      
+    plugin=MdStat.new(nil,{},{})
+    plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__)+'/fixtures/proc_mdstat_raid5.txt'))
 
-      res = plugin.run()
-      assert res[:errors].empty?
-      assert res[:memory][:mdstat_ok]
-      assert res[:reports].any?       
-    end # test_success  
-    
-    def test_error_with_raid_0
-      plugin=MdStat.new(nil,{},{})
-      plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__)+'/fixtures/proc_mdstat_raid0.txt'))
+    res = plugin.run()
+    assert res[:errors].empty?
+    assert res[:memory][:mdstat_ok]
+    assert_equal [{ :active_disks=>5, :spares=>0, :failed_disks=>0}], res[:reports]
+  end # test_success  
+  
+  def test_error_with_raid_0
+    plugin=MdStat.new(nil,{},{})
+    plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__)+'/fixtures/proc_mdstat_raid0.txt'))
 
-      res = plugin.run()
-      assert res[:errors].any?
-    end
+    res = plugin.run()
+    assert_equal 1, res[:errors].length
+    assert_equal "Not applicable for RAID 0", res[:errors].first[:subject]
+  end
+
+  def test_multiple_arrays
+    plugin = MdStat.new(nil, {}, { :monitor_multiple => "true" })
+    plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__) + '/fixtures/proc_mdstat_multiple.txt'))
+
+    response = plugin.run()
+    assert_equal [{ :active_disks => 4, :spares => 0, :failed_disks => 1 }], response[:reports]
+  end
+
+  def test_only_monitor_first_if_monitor_multiple_is_not_set
+    plugin = MdStat.new(nil, {}, {})
+    plugin.stubs(:`).with("cat /proc/mdstat").returns(File.read(File.dirname(__FILE__) + '/fixtures/proc_mdstat_multiple.txt'))
+
+    response = plugin.run()
+    assert_equal [{ :active_disks => 2, :spares => 0, :failed_disks => 0 }], response[:reports]
+  end
 end
