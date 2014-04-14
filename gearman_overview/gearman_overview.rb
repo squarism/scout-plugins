2dc306e42807b2df3a3ebde6e8fa58bfd5a1b019
Michael Mattax <michael@formstack.com>
2014-04-14 09:40:38 -0400

    task to job

--- gearman_overview/gearman_overview.rb
+++ gearman_overview/gearman_overview.rb
@@ -12,10 +12,10 @@ class GearmanOverview < Scout::Plugin
   def build_report
     telnet = Net::Telnet::new("Host" => option(:host), "Port" => option(:port))
     status = telnet.cmd("String" => "status", "Match" => /\n./)
-    counts = { :tasks => 0, :running => 0, :workers => 0}
+    counts = { :jobs => 0, :running => 0, :workers => 0}
     status.split("\n")[0...-1].map do |job|
       task, jobs, running, workers = job.split("\t")
-      counts[:tasks] += jobs.to_i
+      counts[:jobs] += jobs.to_i
       counts[:running] += running.to_i
       counts[:workers] += workers.to_i
     end
