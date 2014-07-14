b652a8d91be7b883278982a0ff4e785b45b4170d
David Anderson <dave@dubkat.com>
2014-07-14 14:27:57 -0600

    Fix minor formatting issues in HerokuDataclip

--- heroku_dataclip/heroku_dataclip.rb
+++ heroku_dataclip/heroku_dataclip.rb
@@ -1,18 +1,18 @@
-class HerokuDataclipPlugin < Scout::Plugin
+class HerokuDataclip < Scout::Plugin
   OPTIONS=<<-EOS
     dataclip_ids:
-      default: "" # a comma-delimited list of dataclip IDs (the string of letters from the url) which return ONLY ONE FIELD AND ROW each
+      default: ""
+      notes: A comma-delimited list of dataclip IDs (the string of letters from the url) which return ONLY ONE FIELD AND ROW each
   EOS
 
   def build_report
     dataclip_ids = option(:dataclip_ids)
     if dataclip_ids.nil? || dataclip_ids.empty? || dataclip_ids !~ /^[a-z,]+$/
-      return error("Invalid or missing option \"dataclip_ids\"",
-        "The \"dataclip_ids\" option is required to be a comma-delimited list of dataclip IDs " +
-          "(the string of letters from the \"dataclips.heroku.com\" url) " +
-          "which return ONLY ONE FIELD AND ROW each " +
-          "(e.g. \"SELECT COUNT(*) AS total_count FROM tablename;\".  " +
-          "Provided value was \"#{dataclip_ids}\""
+      return error('Invalid or missing option "dataclip_ids"',
+        'The "dataclip_ids\" option is required to be a comma-delimited list of dataclip IDs ' +
+          '(the string of letters from the "dataclips.heroku.com" url) ' +
+          'which return ONLY ONE FIELD AND ROW each ' +
+          '(e.g. "SELECT COUNT(*) AS total_count FROM tablename;".'
       )
     end
     dataclip_ids = dataclip_ids.split(',')
