fb991bea4e7c8caa179f4d1f148882a573401b27
Mr. Matt <matt@composition9.com>
2015-02-03 10:33:15 -0700

    docker support for the docker plugin

--- lxc_monitor/lxc_monitor.rb
+++ lxc_monitor/lxc_monitor.rb
@@ -7,14 +7,14 @@ class LXCMonitor < Scout::Plugin
   
   # Units: MB
   def mem
-    File.read('/sys/fs/cgroup/memory/memory.usage_in_bytes').to_f/1024/1024
+    File.read(File.join(directory_prefix, 'sys/fs/cgroup/memory/memory.usage_in_bytes')).to_f/1024/1024
   end
   
   # In %
   def cpu
     res = nil
     # Try to read the cpuacct docker cgroup first, then fall back to lxc
-    cpu_time = File.read('/sys/fs/cgroup/cpuacct/docker/cpuacct.usage_percpu').to_f rescue cpu_time = File.read('/sys/fs/cgroup/cpuacct/lxc/cpuacct.usage_percpu').to_f
+    cpu_time = File.read(File.join(directory_prefix, 'sys/fs/cgroup/cpuacct/docker/cpuacct.usage_percpu')).to_f rescue cpu_time = File.read(File.join(directory_prefix, 'sys/fs/cgroup/cpuacct/lxc/cpuacct.usage_percpu')).to_f
     if @last_run and last_cpu_time = memory(:last_cpu_time)
       elapsed_time = Time.now - @last_run
       cpu_since_last_run = (cpu_time - last_cpu_time)/1000000000 # in nanoseconds
@@ -28,5 +28,10 @@ class LXCMonitor < Scout::Plugin
   
   def containers
     `lxc-ls | sort -u | wc -l`.to_i
+  end
+
+  def directory_prefix
+    # if the scout agent is running in a container with the sys directory mounted under /host
+    File.exists?('/host/sys/fs/cgroup') ? "/host" : "/"
   end
 end
