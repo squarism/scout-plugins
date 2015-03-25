2966c359c4ca1f3a8f782c914fb3cbf7655daf3e
David Anderson <dave@dubkat.com>
2015-03-25 16:11:01 -0600

    Fix logwatcher to safely run on first run and log truncation

--- log_watcher/log_watcher.rb
+++ log_watcher/log_watcher.rb
@@ -33,7 +33,7 @@ class LogWatcher < Scout::Plugin
       return error( "Please provide a path to the log file." )
     end
     
-    `#{@sudo_cmd}test -e #{@log_file_path}`
+    `#{@sudo_cmd}test -r #{@log_file_path}`
     
     unless $?.success?
       error("Could not find the log file", "The log file could not be found at: #{@log_file_path}. Please ensure the full path is correct and your user has permissions to access the log file.") if option("send_error_if_no_log") == "1"
@@ -50,27 +50,34 @@ class LogWatcher < Scout::Plugin
   def build_report
     return if init()
     
+    first_run = memory(:last_bytes).nil?
     last_bytes = memory(:last_bytes) || 0
     current_length = `#{@sudo_cmd}wc -c #{@log_file_path}`.split(' ')[0].to_i
+    read_length = 0
     count = 0
     elapsed_seconds = 0
-    # don't run it the first time
-    if (last_bytes > 0 )
-      read_length = current_length - last_bytes
-      # Check to see if this file was rotated. This occurs when the +current_length+ is less than 
-      # the +last_run+. Don't return a count if this occured.
-      if read_length >= 0
-        # finds new content from +last_bytes+ to the end of the file, then just extracts from the recorded 
-        # +read_length+. This ignores new lines that are added after finding the +current_length+. Those lines 
-        # will be read on the next run.
-        count = `#{@sudo_cmd}tail -c +#{last_bytes+1} #{@log_file_path} | head -c #{read_length} | grep "#{@term}" -#{option(:grep_options).to_s.gsub('-','')}c`.strip.to_f
-        # convert to a rate / min
-        elapsed_seconds = Time.now - @last_run
-        count = count / (elapsed_seconds/60)
-      else
-        count = nil
+
+    if first_run                       # This is the first ever run
+      read_length = 5000000 # 5MB      # Make sure we set a reasonable maximum read_length
+      if current_length > read_length
+        last_bytes = current_length - read_length # Artificially set last_bytes
       end
+    elsif current_length - last_bytes < 0 # Will be negative if the file was rotated/truncated since the last run, so:
+      read_length = current_length        # 1. read the whole file
+      last_bytes = 0                      # 2. from the beginning
+    else
+      read_length = current_length - last_bytes # The file was not truncated and this is not the first run. read_length should be >= 0.
     end
+
+    # finds new content from +last_bytes+ to the end of the file, then just extracts from the recorded
+    # +read_length+. This ignores new lines that are added after finding the +current_length+. Those lines
+    # will be read on the next run.
+    count = `#{@sudo_cmd}tail -c +#{last_bytes+1} #{@log_file_path} | head -c #{read_length} | grep "#{@term}" -#{option(:grep_options).to_s.gsub('-','')}c`.strip.to_f
+
+    # convert to a rate / min
+    elapsed_seconds = Time.now - @last_run
+    count = count / (elapsed_seconds/60)
+
     report(:occurances => count) if count and elapsed_seconds >= 1
     remember(:last_bytes, current_length)
   end
