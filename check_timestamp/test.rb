54ee59aa4c3225829188050e86c14bf9399767b7
Mr. Matt <system9@composition9.com>
2015-04-20 14:32:32 -0600

    fix check_timestamp test

--- check_timestamp/test.rb
+++ check_timestamp/test.rb
@@ -4,12 +4,12 @@ require File.expand_path('../check_timestamp.rb', __FILE__)
 class CheckTimestampTest < Test::Unit::TestCase
   def test_difference
     current_time = Time.now
-    file_time = current_time - 5.minutes
-    Timecop.freeze(Time.now) do
-      File.any_instance.stub(:mtime).returns(current_time)
+    file_time = current_time - 120
+    Timecop.freeze(current_time) do
+      File.any_instance.stubs(:mtime).returns(file_time)
       plugin = CheckTimestamp.new(nil, {}, @options.merge(path: 'check_timestamp/check_timestamp.rb'))
       res = plugin.run
-      assert_equal @res[:difference], 5 * 60
+      assert_equal 120, res[:reports].first[:difference]
     end
   end
 end
