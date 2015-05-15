605a572a0e9dd8af3358855e05f62971768dab02
Derek Haynes <derek.haynes@gmail.com>
2015-05-15 16:17:22 -0600

    Added vertica query metrics

---
+++ vertica_query_metrics/vertica_query_metrics.rb
@@ -0,0 +1,46 @@
+class VerticaQueryMetrics < Scout::Plugin
+  OPTIONS=<<-EOS
+  user:
+    name: Vertica username
+    notes: Specify the username to connect with
+  password:
+    name: Vertica password
+    notes: Specify the password to connect with
+    attributes: password
+  database:
+    name: Vertica database name
+  EOS
+
+	# Raised by #vertica_query when an error occurs.
+  class VerticaConnectionError < Exception
+  end
+
+	def build_report
+		@vertica_command   = option(:vertica_command) || '/opt/vertica/bin/vsql'
+    res = vertica_query("SELECT node_name, node_state, node_address FROM nodes WHERE node_address = '127.0.0.1';")
+    res = vertica_query("SELECT * from query_metrics where node_name = '#{res['node_name']}';")
+    report(res.select { |k| %w(active_user_session_count active_system_session_count running_query_count).include?(k) })
+    counter(:queries, res['executed_query_count'].to_i, :per => :second)
+  end
+
+	private
+
+	def vertica_query(query)
+		# SELECT * from query_metrics;' -A  docker dbadmin
+    # node_name|active_user_session_count|active_system_session_count|total_user_session_count|total_system_session_count|total_active_session_count|total_session_count|running_query_count|executed_query_count
+    # v_docker_node0001|1|2|21|1048|3|1069|1|8
+    # {"node_name"=>"v_docker_node0001", "active_user_session_count"=>"1", "active_system_session_count"=>"2", "total_user_session_count"=>"49", "total_system_session_count"=>"3460", "total_active_session_count"=>"3", "total_session_count"=>"3509", "running_query_count"=>"1", "executed_query_count"=>"22"}
+		result = `#{@vertica_command} -c "#{query}" -A #{option(:database)} #{option(:user)} 2>&1`
+    if $?.success?
+      output = {}
+      puts result
+      keys, values = result.split(/\n/)
+      keys = keys.split('|')
+      values = values.split('|')
+      keys.each_with_index { |key,i| output[key] = values[i]}
+      output
+    else
+      raise VerticaConnectionError, result
+    end
+	end
+end
